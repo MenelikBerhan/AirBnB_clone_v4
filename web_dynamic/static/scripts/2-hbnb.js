29ffdda3ce21ee6321f66084a064e4f5b5ed1690
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-11-05 03:00:21 +0300

    Add 1-hbnb.js's content to 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,15 @@
-// js for updating api_status
-function fetchApiStatus () {
+// js for updating api_status and selected amenities list
+function myfun () {
+  const amenities = {};
+  $('li input[type=checkbox]').change(function () {
+    if (this.checked) {
+      amenities[this.dataset.name] = this.dataset.id;
+    } else {
+      delete amenities[this.dataset.name];
+    }
+    $('div.amenities h4').text(Object.keys(amenities).sort().join(', '));
+  });
+
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, (data) => {
     if (data.status === 'OK') $('div#api_status').addClass('available');
@@ -7,4 +17,4 @@ function fetchApiStatus () {
   });
 }
 
-$(document).ready(fetchApiStatus);
+$(document).ready(myfun);
