b59f8c85b21a89b259c6e5ac7f70ab61cbe62da4
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-11-05 22:34:28 +0300

    Add filter for places based on selected amenities

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,6 +1,7 @@
 /*
-  js for updating api_status, setting selected amenities list from checkboxes
-  and fetching places from api
+  js for updating api_status, setting selected amenities list from checkboxes,
+  fetching places from api on start and also when search button is clicked
+  based on selected amenities.
 */
 
 function myfun () {
@@ -51,6 +52,45 @@ function myfun () {
         $('section.places').append(placeArticle);
       }
     }
+  });
+
+  // fetches places from api based on selected amenities
+  $('button[type=button]').on('click', function () {
+    const amenitIds = [];
+    Object.values(selectedAmenities).forEach(function (amenitId) {
+      amenitIds.push(amenitId);
+    });
+
+    const searchUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+    $.ajax({
+      url: searchUrl,
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify({ amenities: amenitIds }),
+      contentType: 'application/json;',
+      success: function (data) {
+        const places = data.sort((a, b) => a.name.localeCompare(b.name));
+        $('section.places').empty();
+        for (const place of places) {
+          const placeArticle = $('<article></article');
+
+          const titleBox = $('<div></div>').addClass('title_box');
+          titleBox.append($('<h2>' + place.name + '</h2>'));
+          titleBox.append($('<div class="price_by_night">$' + place.price_by_night + '</div>'));
+
+          const information = $('<div></div>').addClass('information');
+          information.append('<div class="max_guest">' + place.max_guest + 'Guest' + (place.max_guest !== 1 ? 's' : '') + '</div>');
+          information.append('<div class="number_rooms">' + place.number_rooms + 'Bedroom' + (place.number_rooms !== 1 ? 's' : '') + '</div>');
+          information.append('<div class="number_bathrooms">' + place.number_bathrooms + 'Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') + '</div>');
+
+          const description = $('<div class="description">' + place.description + '</div>');
+
+          placeArticle.append(titleBox, information, description);
+
+          $('section.places').append(placeArticle);
+        }
+      }
+    });
   });
 }
 
